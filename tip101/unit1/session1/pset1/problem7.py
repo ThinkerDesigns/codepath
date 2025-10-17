63a73782234ad8ba16de76564d826743f7240af5
Dhruv <62044304+ThinkerDesigns@users.noreply.github.com>
2025-10-17 09:00:00 +0000

    2026-06-03 update

---
+++ tip101/unit1/session1/pset1/problem7.py
@@ -0,0 +1,10 @@
+def what_time_is_it(hour):
+    if hour == 2:
+        return "taco time 🌮"
+    elif hour == 12:
+        return "peanut butter jelly time 🥪"
+    else:
+        return "nap time 😴"
+print(what_time_is_it(2))
+print(what_time_is_it(7))
+print(what_time_is_it(12))
