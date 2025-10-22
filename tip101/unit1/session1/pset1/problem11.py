2c32128a373a4213498b72763fe0b956de204a3b
Dhruv <62044304+ThinkerDesigns@users.noreply.github.com>
2025-10-22 09:00:00 +0000

    2026-06-03 update

--- tip101/unit1/session1/pset1/problem11.py
+++ tip101/unit1/session1/pset1/problem11.py
@@ -1,7 +1,4 @@
 def counter(stop):
-    for i in range(stop + 1):
-        if i == 0:
-            pass
-        else:    
-            print(i)
+    for i in range(1, stop + 1):
+        print(i)
 counter(7)
---
+++ tip101/unit1/session1/pset1/problem12.py
@@ -0,0 +1,7 @@
+def sum_ten():
+    i = 0
+    for x in range(1,11):
+        i += x
+    print(i)
+output = sum_ten()
+print(output)
