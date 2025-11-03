25f98d927c419b33a68070a8c06b49e2047ab3ba
Dhruv <62044304+ThinkerDesigns@users.noreply.github.com>
2025-11-03 09:00:00 +0000

    2026-06-03 update

--- tip101/unit1/session1/pset1/problem15.py
+++ tip101/unit1/session1/pset1/problem15.py
@@ -1,15 +1,14 @@
-# WIP - only finds the first negative and exits
+# WIP - can't print None for list with no negatives
 def print_negatives(lst):
     i = 0
-    while (i <= len(lst)):
-        for x in range(len(lst)):
-            if lst[x] > 0:
-                continue
-            elif lst[x] < 0:
-                return(lst[x])
-            i += 1
+    for x in range(len(lst)):
+        if lst[x] < 0:
+            print(lst[x])
+            i +=1
+        elif lst[x] > 0:
+            continue
                 
 example = [3,-2,2,1,-5]
 example2 = [1,2,3,4,5]
-print(print_negatives(example))
-#print(print_negatives(example2))
+print_negatives(example)
+print_negatives(example2)
