018cfbdf2f61fed2609829d6f11ee89e7cb149bd
Dhruv <62044304+ThinkerDesigns@users.noreply.github.com>
2025-11-26 09:00:00 +0000

    2026-06-11 update

--- tip101/unit2/session2/pset1/problem2.py
+++ tip101/unit2/session2/pset1/problem2.py
@@ -23,6 +23,7 @@ def common_keys(dict1,dict2):
     for key in dict1:
         if key in dict2:
             result.append(key)
+    return result
 dict1 = {"a": 1, "b": 2, "c": 3}
 dict2 = {"b": 4, "c": 5, "d": 6}
 common_list = common_keys(dict1, dict2)
