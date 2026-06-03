48fd27c8f4b2f0a29046814e8e3262187e919091
Dhruv Bhadauriya <62044304+ThinkerDesigns@users.noreply.github.com>
2026-06-03 15:31:24 -0700

    2026-06-03 update

---
+++ tip101/unit1/session1/pset2/problem6.py
@@ -0,0 +1,4 @@
+# WIP - need to figure out how to give the values of rock, paper, and scissors without manually defining all 8 possibilites
+def rock_paper_scissors(player1,player2):
+    if player1 == player2:
+        print("It's a tie!")
