dbb7afe919066540927c668b12f29410439d293f
Dhruv Bhadauriya <62044304+ThinkerDesigns@users.noreply.github.com>
2026-06-04 17:14:54 -0700

    2026-06-04 update

--- tip101/unit1/session2/pset1/problem2.py
+++ tip101/unit1/session2/pset1/problem2.py
@@ -1,3 +1,6 @@
+# Understand: Input a list and output the items in the list
+# Plan: We should write a function that takes in a list and uses a for loop to go through all the items, double them, and print them on a new line
+# Implement: Run a for loop, double each item, and just print out each doubled item using iterations
 def doubled(lst):
     for x in range(len(lst)):
         print(lst[x] * 2)
