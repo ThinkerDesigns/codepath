1288d6b547d32a6dff33eec187589612f8872e6f
Dhruv Bhadauriya <62044304+ThinkerDesigns@users.noreply.github.com>
2026-06-14 01:26:20 -0700

    2026-06-14 update

--- tip101/unit2/session1/pset1/problem6.py
+++ tip101/unit2/session1/pset1/problem6.py
@@ -1,8 +1,19 @@
-# WIP - need to find out how to make the string of ABCDF into seperate ints
 def calculate_gpa(report_card):
     gpa = 0
+    test = {}
+    for i in range(len(report_card)):
+        if list(report_card.values())[i] == "A":
+            test[list(report_card)[i]] = 4
+        elif list(report_card.values())[i] == "B":
+            test[list(report_card)[i]] = 3
+        elif list(report_card.values())[i] == "C":
+            test[list(report_card)[i]] = 2
+        elif list(report_card.values())[i] == "D":
+            test[list(report_card)[i]] = 1
+        else:
+            test[list(report_card)[x]] = 0
     for x in range(len(report_card)):
-        gpa = gpa + list(report_card)[x]
+        gpa = gpa + list(test.values())[x]
     gpa = gpa / len(report_card)
     return gpa
 report_card = {"Math": "A", "Science": "C", "History": "A", "Art": "B", "English": "B", "Spanish": "A"}
