5983907115ec0c778151fbe5ff43e965718bd18e
Dhruv Bhadauriya <62044304+ThinkerDesigns@users.noreply.github.com>
2026-07-19 01:58:58 -0700

    2026-07-19 update

---
+++ tip101/unit7/session1/pset1/problem4.py
@@ -0,0 +1,9 @@
+def is_power_of_two(n):
+	if n == 2:
+		return True
+	elif n % 2 != 0:
+		return False
+	else:
+		return is_power_of_two(n//2)
+print(is_power_of_two(16))
+print(is_power_of_two(18))
